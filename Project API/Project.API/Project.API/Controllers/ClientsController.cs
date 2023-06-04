37d6fbad32ac8adce44bab32584023727708c287
Talpiga Andrei-Vlad <vlad.talpiga@gmail.com>
2023-06-04 21:40:47 +0300

    HttpGet+Add

---
+++ Project API/Project.API/Project.API/Controllers/ClientsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project.API.Data;
+using Project.API.Models;
+
+namespace Project.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientsController : Controller
+    {
+        private readonly ProjectDbContext _projectDbContext;
+        public ClientsController(ProjectDbContext projectDbContext)
+        {
+            _projectDbContext = projectDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllClients()
+        {
+            var clients = await _projectDbContext.Clients.ToListAsync();
+            return Ok(clients);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddClient([FromBody] Client clientRequest)
+        {
+            clientRequest.Id = Guid.NewGuid();
+            await _projectDbContext.Clients.AddAsync(clientRequest);
+            await _projectDbContext.SaveChangesAsync();
+
+            return Ok(clientRequest);
+        }
+    }
+}
