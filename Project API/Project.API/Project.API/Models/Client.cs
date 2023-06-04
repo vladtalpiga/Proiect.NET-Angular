5dcb8311cd0f1e60877aa76fd6747ef81760de9d
Talpiga Andrei-Vlad <vlad.talpiga@gmail.com>
2023-06-04 03:41:07 +0300

    s

--- Project API/Project.API/Project.API/Models/Client.cs
+++ Project API/Project.API/Project.API/Models/Client.cs
@@ -3,8 +3,8 @@
     public class Client
     {
         public Guid Id { get; set; }
-        public string? Name { get; set; }
-        public string? Email { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
         public int Age { get; set; }
         public long Money { get; set; }
     }
